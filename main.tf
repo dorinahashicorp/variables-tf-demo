8aafb6609571e6a3a2e3365dfd2481e74521747b
dorinahashicorp <94868032+dorinahashicorp@users.noreply.github.com>
2023-11-06 19:52:35 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,6 +9,7 @@ terraform {
 provider "tfe" {
   hostname = "app.terraform.io"
   organization = "Infragoose"
+  token = "8G5WxgCrIJXOHQ.atlasv1.GvjCVw85lhcUv6TazJGiPHy9wN2HLeaJX9zBdMdPzMDehFnBzvWXSbGQYXnGXPpB0jc"
 }
 
 resource "tfe_workspace" "test" {
